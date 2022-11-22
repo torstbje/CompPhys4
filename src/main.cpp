9a8a6f8dc8421e0e1fc164404a12c13607592c03
KlogeBukser <imvacant0923@gmail.com>
2022-11-22 05:54:03 +0100

    parallelised values for different temperatures

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,23 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <cmath>
 #include <ctime>
+#include <cassert>
 #include "omp.h"
+
 
 
 #include "Particle.hpp"
 #include "Lattice.hpp"
 #include "funcs.hpp"
 
+
 #define ERR_MISSING_PARAMS 1
+#define ERR_INVALID_LATTICE 2
 
 // Contains the free functions of this project.
 using namespace ising;
@@ -24,37 +29,100 @@ int main(const int argc, const char *argv[])
     // Random seed
     srand(time(NULL));
 
-    if (argc < 6) {
+    if (argc < 8) {
         std::cout << "\nMissing input parameters! (" << argc - 1 << " parameters was included.) \n"
             "Necessary parameters:\n"
             "- Aligned spins ('1'/'0') \n"
             "- L: length of lattice \n"
-            "- T: Temperature (double, unit [J/k_B]) \n"
+//            "- T: Temperature (double, unit [J/k_B]) \n"
             "- Number of cycles (integer) \n"
-            "- Filename";
+            "- Min temp \n"
+            "- Max temp \n"
+            "- Filename"
+            "- Number of steps for temperature run";
             
         return ERR_MISSING_PARAMS;
     }
 //
-    bool aligned = atoi(argv[1]);
-    int dim = atoi(argv[2]);
-    double temperature = std::stod(argv[3]);
+    std::string bjelland = argv[1];
+    bool aligned = atoi(argv[2]);
+    int dim = atoi(argv[3]);
+//    double temperature = std::stod(argv[3]);
     int cycles = atoi(argv[4]);
-    std::string filename = argv[5];
+//    std::string filename = argv[5];
+    double min_temp = atof(argv[5]);
+    double max_temp = atof(argv[6]);
+    int n_steps = atoi(argv[7]);
 
     int n_particles = dim * dim;
+    
+    std::string filename1, filename2;
+    std::string a = argv[1], b = argv[2], c = argv[3], d = argv[4], e = argv[5];
+    std::string f = argv[6], g = argv[7];
+    filename1 = a + b + c + d + e + "-1.txt";
+    filename2 = a + b + c + d + e + "-2.txt";
+    filename1 = "textfiles/" + b + "-" + c + "-" + d +".txt";
+    filename2 = "textfiles/t-cv-chi-" +b+ "-"+c+ "-"+d+ "-"+e+ "-"+f+ "-"+g+ ".txt";
     
     std::vector<Particle> particles(0);
     fill_particle_list(n_particles,particles,aligned);
 
 
-    Lattice lattice(dim,particles,temperature);
+//    Lattice lattice(dim,particles,temperature);
 
     
+//    if (!lattice.test_lattice()) {
+//        return ERR_INVALID_LATTICE;
+//    }
     
-    if (!lattice.test_lattice()) {
-        return 1;
+//    mcmc_calculate(lattice,cycles,filename1);
+    
+//    double min_temp = 2.0, max_temp = 2.5;
+    double curr_temp = min_temp, dtemp = (max_temp - min_temp)/double(n_steps);
+   
+    
+    std::cout << "======================== temperature ========================" << std::endl;
+    
+    std::ofstream outfile;
+    outfile.open(filename2);
+    assert(outfile);
+    std::cout << filename2 << std::endl;
+   
+    
+    #pragma omp parallel for
+
+    for (int i=0;i<n_steps;i++) {
+        double values[4];
+        values[0] = -999;
+        values[1] = -999;
+        values[2] = -999;
+        values[3] = -999;
+
+        std::cout << i << std::endl;
+        Lattice lattice(dim,particles,curr_temp);
+        assert(lattice.test_lattice());
+        mc_phase(lattice, cycles, 100, values);
+        outfile << curr_temp << "," << values[0] << "," << values[1] << "," << values[2] << "," << values[3] << endl;
+        #pragma omp atomic
+        curr_temp += dtemp;
     }
+    outfile.close();
     
-    mcmc_calculate(lattice,cycles,filename);
+//        double values[4];
+//        values[0] = -999;
+//        values[1] = -999;
+//        values[2] = -999;
+//        values[3] = -999;
+//    for (int i=0;i<n_steps;i++) {
+//        std::cout << i << std::endl;
+//        Lattice lattice(dim,particles,curr_temp);
+//        if (!lattice.test_lattice()) {
+//            return ERR_INVALID_LATTICE;
+//        }
+//        mc_phase(lattice, cycles, 100, values);
+//        outfile << curr_temp << "," << values[0] << "," << values[1] << "," << values[2] << "," << values[3] << endl;
+//
+//        curr_temp += dtemp;
+//    }
+//    outfile.close();
 }
---
+++ src/plot-temp.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import warnings
+import sys
+
+
+filetag = sys.argv[1]
+filename = "textfiles/t-cv-chi-" + filetag + ".txt"
+warnings.filterwarnings("ignore" )
+
+# for temperature in temperatures:
+data = pd.read_csv(filename,header=None)
+
+temp = data[0]
+mean_energy = data[1]
+mean_magnetisation = data[2]
+cv = data[3]
+chi = data[4]
+
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10,10))
+
+ax[0][0].plot(temp, cv)
+ax[0][0].set_title("Specific heat capacity vs Temperature")
+ax[0][0].set_xlabel(r"Tmepperature $(J/k_B)$")
+ax[0][0].set_ylabel(r"Specific heat capacity $(k_B)$")
+
+ax[0][1].plot(temp, chi)
+ax[0][1].set_title("Magenetic susceptibility vs Temperature")
+ax[0][1].set_xlabel(r"Tmepperature $(J/k_B)$")
+ax[0][1].set_ylabel(r"Magenetic susceptibility $(1/J)$")
+
+ax[1][0].plot(temp, mean_energy)
+ax[1][0].set_title("Mean Energy vs Temperature")
+ax[1][0].set_xlabel(r"Tmepperature $(J/k_B)$")
+ax[1][0].set_ylabel(r"Mean Energy $(J)$")
+
+ax[1][1].plot(temp, mean_magnetisation)
+ax[1][1].set_title("Mean Absolute Magnetisation vs Temperature")
+ax[1][1].set_xlabel(r"Tmepperature $(J/k_B)$")
+ax[1][1].set_ylabel(r"Mean Absolute Magnetisation $(unitless)$")
+
+plt.savefig("plots/temp" + filetag + ".pdf")
+plt.close()
