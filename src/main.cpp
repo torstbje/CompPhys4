c2efe06cb46dc9b1c21c49dcaadf92402cfccb57
torstbje <torstbje@uio.no@users.noreply.github.com>
2022-11-22 06:42:29 +0100

    Added cmd param for instructions on runnning code. Put parallization part inti 'critical'. Also made instructions 'simple' and 'burn'

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 #include <cmath>
 #include <ctime>
 #include <cassert>
+#include <fstream>
 #include "omp.h"
-
 
 
 #include "Particle.hpp"
@@ -24,18 +24,154 @@
 using namespace ising;
 using namespace std;
 
-int main(const int argc, const char *argv[])
+int main(const int argc, const char* argv[])
 {
     // Random seed
     srand(time(NULL));
 
+    if (argc < 4) {
+        // These parameters are needed for all program runs
+        std::cout << "\nMissing input parameters! (" << argc - 1 << " parameters was included.) \n"
+            "All calls of this program should include the following parameters:\n"
+            "- Run time instruction: ('simple' / 'burn' / 'distribution' / 'critical' )\n"
+            "- Aligned spins ('1'/'0') \n"
+            "- Number of cycles (integer) \n";
+        return ERR_MISSING_PARAMS;
+    }
+
+    std::string instruction = argv[1];
+    bool aligned = atoi(argv[2]);
+    int cycles = atoi(argv[3]);
+
+    if (instruction == "simple") {
+        double t_min = 1.0;
+        double t_max = 2.4;
+        double t_step = 0.01;
+        std::ofstream outfile;
+        outfile.open("textfiles/simple.txt");
+        for (double temperature = t_min; temperature < t_max; temperature += t_step) {
+            std::vector<Particle> particles(0);
+            fill_particle_list(4, particles, aligned);
+
+            Lattice lattice(2, particles, temperature);
+
+            double values[4];
+            mc_phase(lattice, cycles, 100, values);
+
+
+            for (int i = 0; i < 4; i++) {
+                outfile << values[i] << " , ";
+            }
+            outfile << temperature << endl;
+
+        }
+        outfile.close();
+
+    }
+    else if (instruction == "burn") {
+        // Checks if the temperature is included as well.
+        if (argc < 6) {
+            std::cout << "\nMissing input parameters! (" << argc - 1 << " parameters was included.) \n";
+            wrong_cmd_input(instruction);
+            return ERR_MISSING_PARAMS;
+        }
+
+        int dim = std::atoi(argv[4]);
+        double temperature = std::stod(argv[5]);
+
+        int n_particles = dim * dim;
+        std::vector<Particle> particles(0);
+        fill_particle_list(n_particles, particles, aligned);
+
+        Lattice lattice(dim, particles, temperature);
+        std::string filename = make_filename(instruction, aligned, temperature);
+        mc_find_burn(lattice, cycles, filename);
+    }
+
+    else if (instruction == "critical") {
+        if (argc < 8) {
+            std::cout << "\nMissing input parameters! (" << argc - 1 << " parameters was included.) \n";
+            wrong_cmd_input(instruction);
+            return ERR_MISSING_PARAMS;
+        }
+        int cycles = atoi(argv[3]);
+        int dim = atoi(argv[4]);
+        double min_temp = atof(argv[5]);
+        double max_temp = atof(argv[6]);
+        int n_steps = atoi(argv[7]);
+
+        int n_particles = dim * dim;
+
+        std::string filename1, filename2;
+        std::string a = argv[1], b = argv[2], c = argv[4], d = argv[3], e = argv[5];
+        std::string f = argv[6], g = argv[7];
+
+        filename1 = "textfiles/" + b + "-" + c + "-" + d + ".txt";
+        filename2 = "textfiles/t-cv-chi-" + b + "-" + c + "-" + d + "-" + e + "-" + f + "-" + g + ".txt";
+
+        std::vector<Particle> particles(0);
+        fill_particle_list(n_particles, particles, aligned);
+
+
+        double curr_temp = min_temp, dtemp = (max_temp - min_temp) / double(n_steps);
+
+
+        std::cout << "======================== temperature ========================" << std::endl;
+
+        std::ofstream outfile;
+        outfile.open(filename2);
+        assert(outfile);
+        std::cout << filename2 << std::endl;
+
+
+#pragma omp parallel for
+
+        for (int i = 0; i < n_steps; i++) {
+            double values[4];
+            values[0] = -999;
+            values[1] = -999;
+            values[2] = -999;
+            values[3] = -999;
+
+            std::cout << i << std::endl;
+            Lattice lattice(dim, particles, curr_temp);
+            assert(lattice.test_lattice());
+            mc_phase(lattice, cycles, 100, values);
+            outfile << curr_temp << "," << values[0] << "," << values[1] << "," << values[2] << "," << values[3] << endl;
+#pragma omp atomic
+            curr_temp += dtemp;
+        }
+        outfile.close();
+
+        //        double values[4];
+        //        values[0] = -999;
+        //        values[1] = -999;
+        //        values[2] = -999;
+        //        values[3] = -999;
+        //    for (int i=0;i<n_steps;i++) {
+        //        std::cout << i << std::endl;
+        //        Lattice lattice(dim,particles,curr_temp);
+        //        if (!lattice.test_lattice()) {
+        //            return ERR_INVALID_LATTICE;
+        //        }
+        //        mc_phase(lattice, cycles, 100, values);
+        //        outfile << curr_temp << "," << values[0] << "," << values[1] << "," << values[2] << "," << values[3] << endl;
+        //
+        //        curr_temp += dtemp;
+        //    }
+        //    outfile.close();
+    }
+}
+
+
+    /*
     if (argc < 8) {
         std::cout << "\nMissing input parameters! (" << argc - 1 << " parameters was included.) \n"
             "Necessary parameters:\n"
+            "- Run time instruction: ('simple' / 'burn' / 'distribution' / 'critical' )"
             "- Aligned spins ('1'/'0') \n"
-            "- L: length of lattice \n"
-//            "- T: Temperature (double, unit [J/k_B]) \n"
             "- Number of cycles (integer) \n"
+            "- L: length of lattice \n"
             "- Min temp \n"
             "- Max temp \n"
             "- Filename"
@@ -43,13 +179,9 @@ int main(const int argc, const char *argv[])
             
         return ERR_MISSING_PARAMS;
     }
-//
-    std::string bjelland = argv[1];
-    bool aligned = atoi(argv[2]);
-    int dim = atoi(argv[3]);
-//    double temperature = std::stod(argv[3]);
-    int cycles = atoi(argv[4]);
-//    std::string filename = argv[5];
+    else
+    int cycles = atoi(argv[3]);
+    int dim = atoi(argv[4]);
     double min_temp = atof(argv[5]);
     double max_temp = atof(argv[6]);
     int n_steps = atoi(argv[7]);
@@ -125,4 +257,4 @@ int main(const int argc, const char *argv[])
 //        curr_temp += dtemp;
 //    }
 //    outfile.close();
-}
+}*/
